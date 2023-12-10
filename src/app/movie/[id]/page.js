8ba01015dd34e4dbc71114421020993b09520e63
probeshnath <probeshn@gmail.com>
2023-12-10 14:20:10 +0600

    info fetch from rapid api

---
+++ src/app/movie/[id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>i'm daynamic route</div>
+  )
+}
+
+export default page
--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,8 +1,40 @@
+import Link from 'next/link'
 import React from 'react'
+import MovieCard from '../components/MovieCard';
+import styles from '@/app/styles/common.module.css'
 
-const Movie = () => {
+const Movie = async () => {
+
+  const url = process.env.RAPID_KEY;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'b9879fac2cmsh2fbc6e841555a6ep12bccajsn8d4c3e121701',
+      'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+    }
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+  // console.log(data)
+
+
   return (
-    <div>Movie</div>
+    <>
+      <section className={`${styles.movieSection}`} >
+        <div className={styles.container} >
+
+
+          <h1>Series & Movie</h1>
+          {
+            main_data.map((curtElem) => (
+              <MovieCard key={curtElem.id} curtElem={curtElem} />
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
