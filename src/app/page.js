e470198de870492bf3e7852b83c24afea9e02b23
probeshnath <probeshn@gmail.com>
2023-12-10 12:31:50 +0600

    home page added

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HeroSection from "./components/HeroSection"
 const page = () => {
   return (
     <div>
-      <HeroSection />
+      <HeroSection title="This is the title ection" imageUrl="https://cdni.iconscout.com/illustration/premium/thumb/man-is-selecting-design-shape-8566345-6882208.png?f=webp" />
     </div>
   )
 }
